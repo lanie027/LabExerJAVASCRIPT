964adc23c67f47ce61f14806ed8a1ade306623f4
Princess III <melanieardientes0213@gmail.com>
2023-05-10 20:22:33 -0700

    initial commit

---
+++ fav.js
@@ -0,0 +1,14 @@
+// Ask the user for the age of their favorite artist
+let artistAge = prompt("What is the age of your favorite artist?");
+
+// Convert the user's input to a number
+artistAge = Number(artistAge);
+
+// Compare the artist's age to 25
+if (artistAge < 25) {
+  console.log("They're " + artistAge + " years old and they're younger than Google");
+} else if (artistAge > 25) {
+  console.log("They're " + artistAge + " years old and they're younger than Google");
+} else {
+  console.log("They're " + artistAge + " years old and they're younger than Google");
+}
